032b665ca5031ca34b454c9286873fbfd5832615
Maria Luiza Ribeiro <163929189+MariaRuasCode@users.noreply.github.com>
2024-11-23 14:53:54 -0300

    fix - docker, adicionando arquivos para fazer os endpoints

--- Backend/app/index.js
+++ Backend/app/index.js
@@ -5,3 +5,26 @@ const port = 8080;
 app.get('/', (req, res) => res.send('ALOU!!!'));
 
 app.listen(port, () => console.log(`App running on http://localhost:${port}`));
+
+
+/**Usuario
+ *  1. Registro
+ *  2. Login
+ *  3. Deletar
+ * 
+ * 4. get users
+ * 
+  */
+
+/**Produto 
+ *  1. Get todos os produtos
+ *  2. Deletar produto
+ *  3. Adicionar Produto
+ */
+
+/** Pedido
+ *  1. Criar um pedido
+ *  2. Excluir Pedido
+ *  3. Get Pedido
+ *  4. 
+ */
